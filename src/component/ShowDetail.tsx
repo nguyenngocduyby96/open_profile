6c0c6d56a2740564b97624a44292a09bf10b48a7
Duy Nguyen <duyby96@gmail.com>
2023-03-10 12:06:30 +0700

    commit

--- src/component/ShowDetail.tsx
+++ src/component/ShowDetail.tsx
@@ -2,6 +2,7 @@ import { Button, Input, Modal, Select, Table, Tag } from "antd";
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
+import { listProxy } from "../constant";
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
@@ -24,8 +25,14 @@ type Props = {
 const filterKey = "nguyenngocduyby96akldjfklajdflk";
 export const ShowDetail: React.FC<Props> = ({ data, apiUrl }) => {
   const [openModal, setOpenModal] = React.useState(false);
+  const [openModalProxy, setOpenModalProxy] = React.useState(false);
   const [startNumber, setStartNumber] = React.useState("");
   const [endNumber, setEndNumber] = React.useState("");
+
+  const [profileStart, setProfileStart] = React.useState("");
+  const [profileEnd, setProfileEnd] = React.useState("");
+  const [proxyStart, setProxyStart] = React.useState("");
+  const [proxyEnd, setProxyEnd] = React.useState("");
   const [filterGroup, setFilterGroup] = React.useState(
     "nguyenngocduyby96akldjfklajdflk"
   );
@@ -48,12 +55,21 @@ export const ShowDetail: React.FC<Props> = ({ data, apiUrl }) => {
 
   const columns = [
     {
-      title: "Name",
+      title: "Id",
+      key: "index",
+      render: (text: any, record: any, index: any) => {
+        const id = record.id;
+        const idx = currentData.findIndex((i) => i.id === id);
+        return idx + 1;
+      },
+    },
+    {
+      title: "Tên",
       dataIndex: "name",
       key: "name",
     },
     {
-      title: "Group Name",
+      title: "Group",
       dataIndex: "group_name",
       key: "group_name",
     },
@@ -62,7 +78,45 @@ export const ShowDetail: React.FC<Props> = ({ data, apiUrl }) => {
       dataIndex: "id",
       key: "id",
     },
+    {
+      title: "Proxy",
+      dataIndex: "proxy",
+      key: "proxy",
+    },
   ];
+
+  const columnsProxy = [
+    {
+      title: "Id",
+      key: "index",
+      render: (text: any, record: any, index: any) => {
+        const proxy = record.proxy;
+        const i = listProxy.findIndex((i) => i === proxy);
+        return i + 1;
+      },
+    },
+    {
+      title: "Tên Proxy",
+      dataIndex: "proxy",
+      key: "proxy",
+    },
+    {
+      title: "Đang được sử dụng ở",
+      dataIndex: "used",
+      key: "used",
+    },
+  ];
+
+  const listProxyTable = React.useMemo(() => {
+    return listProxy.reduce((acc: any, key: any, i: number) => {
+      console.log("index", i);
+      const index = data.findIndex((item) => item.proxy === key);
+      if (index === -1) {
+        return [...acc, { proxy: key, used: "" }];
+      }
+      return [...acc, { proxy: key, used: data[index].name }];
+    }, []);
+  }, [listProxy]);
 
   const callOpenProfile = async (id: string) => {
     const response = await axios.get(`${apiUrl}/v2/start?profile_id=${id}`);
@@ -93,6 +147,26 @@ export const ShowDetail: React.FC<Props> = ({ data, apiUrl }) => {
     });
   };
 
+  const callUpdateProxy = async (id: string, proxy: string) => {
+    console.log({id})
+    console.log({proxy})
+    await axios.get(`${apiUrl}/v2/update?id=${id}&proxy=${proxy}`);
+  };
+
+  const handleUpdateProxy = () => {
+    const listProfile = currentData.slice(
+      Number(profileStart) - 1,
+      Number(profileEnd)
+    );
+    const listProxyUpdate = listProxy.slice(
+      Number(proxyStart) - 1,
+      Number(proxyEnd)
+    );
+    listProfile.forEach((element, index) => {
+      callUpdateProxy(element.id, listProxyUpdate[index]);
+    });
+  };
+
   return (
     <Wrapper>
       <div
@@ -119,6 +193,13 @@ export const ShowDetail: React.FC<Props> = ({ data, apiUrl }) => {
           />
         </div>
         <div>
+          <Button
+            type="default"
+            style={{ marginRight: 10 }}
+            onClick={() => setOpenModalProxy(true)}
+          >
+            List Proxy
+          </Button>
           <Button
             type="default"
             style={{ marginRight: 10 }}
@@ -162,6 +243,70 @@ export const ShowDetail: React.FC<Props> = ({ data, apiUrl }) => {
               onChange={(e) => setEndNumber(e.target.value)}
             ></Input>
           </div>
+        </Modal>
+      )}
+
+      {openModalProxy && (
+        <Modal
+          style={{ top: 10 }}
+          width={"50%"}
+          title="List Proxy"
+          open={openModalProxy}
+          onOk={() => setOpenModalProxy(false)}
+          onCancel={() => setOpenModalProxy(false)}
+          footer={[
+            <Button key="back" onClick={() => setOpenModalProxy(false)}>
+              Đóng
+            </Button>,
+          ]}
+        >
+          <div style={{ display: "flex" }}>
+            <div style={{ marginRight: 20 }}>
+              Nhập Profile từ số:
+              <Input
+                type="number"
+                value={profileStart}
+                onChange={(e) => setProfileStart(e.target.value)}
+              ></Input>
+            </div>
+            <div>
+              Đến Profile số:
+              <Input
+                type="number"
+                value={profileEnd}
+                onChange={(e) => setProfileEnd(e.target.value)}
+              ></Input>
+            </div>
+          </div>
+          <div style={{ display: "flex" }}>
+            <div style={{ marginRight: 20 }}>
+              Nhập Proxy từ số:
+              <Input
+                type="number"
+                value={proxyStart}
+                onChange={(e) => setProxyStart(e.target.value)}
+              ></Input>
+            </div>
+            <div>
+              Đến Proxy số:
+              <Input
+                type="number"
+                value={proxyEnd}
+                onChange={(e) => setProxyEnd(e.target.value)}
+              ></Input>
+            </div>
+          </div>
+          <div style={{ padding: 10, display: "flex" }}>
+            <Button type="primary" onClick={() => handleUpdateProxy()}>
+              Update proxy
+            </Button>
+          </div>
+          <Table
+            rowKey={"proxy"}
+            style={{ width: "90%" }}
+            dataSource={listProxyTable}
+            columns={columnsProxy}
+          />
         </Modal>
       )}
     </Wrapper>
